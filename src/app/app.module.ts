b0f2310cc85b466876ab135046edc4890de9a077
Kenneth Ekandem <kennyekandem@gmail.com>
2021-04-20 09:33:24 +0100

    update to layouts and auth pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { BlankComponent } from './components/layouts/blank/blank.component';
+import { DefaultComponent } from './components/layouts/default/default.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    BlankComponent,
+    DefaultComponent
   ],
   imports: [
     BrowserModule,
